12f7107b9c17f5b1a2e945cfc075f75e65cc2e55
Eduardo Catalán <mcatalan@cartacorp.net>
2023-06-07 16:16:07 -0600

    correxion en modulo de cpu

--- modules/cpu/cpu_grupo11.c
+++ modules/cpu/cpu_grupo11.c
@@ -10,100 +10,78 @@
 #include <linux/fs.h>
 
 struct list_head *p;
-struct task_struct *proceso, ts, *tsk;
+struct task_struct *processes, ts, *tsk;
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Monitor modulo CPU");
 MODULE_AUTHOR("Marvin Eduardo Catalán Véliz, Sara Paulina Medrano Cojulún");
 
 
-static int write_file(struct seq_file *archivo, void *v){
-    int pros = 0;
-    int pros2 = 0;
-    seq_printf(archivo, "{\n");
-    seq_printf(archivo, "\"procesos\":[\n");
+static int write_file(struct seq_file *file, void *v){
+
+    seq_printf(file, "{\n");
+    seq_printf(file, "\"procesos\":[\n");
 
     bool seconditerative=false;
 
-    for_each_process(proceso)
-    {
+    for_each_process(processes){
 
-        //seq_printf(archivo, "{\n\"proceso%i\":{\n", pros2);
         if(seconditerative){
-        seq_printf(archivo, ",");    
+        seq_printf(file, ",");    
         }else{
             seconditerative=true;
         }
 
-        seq_printf(archivo, "{\n");
-        seq_printf(archivo, "\"PID\": %d, \n", proceso->pid);
-        seq_printf(archivo, "\"Nombre\": \"%s\", \n", proceso->comm);
-        seq_printf(archivo, "\"Usuario\": %d, \n", (int) proceso->sessionid);
-        //seq_printf(archivo, "\"Memory\": \"%lu\", \n",(unsigned long) proceso->mm->start_data);
-        //printf("ULONG_MAX   :   %lu\n", (unsigned long) ULONG_MAX);
-        seq_printf(archivo, "\"Memory\": \"%d\", \n",__kuid_val(proceso->real_cred->uid)); //__kuid_val(s->real_cred->uid)
-        seq_printf(archivo, "\"Estado\": %ld} \n", proceso->state);
+        seq_printf(file, "{\n");
+        seq_printf(file, "\"PID\": %d, \n", processes->pid);
+        seq_printf(file, "\"Nombre\": \"%s\", \n", processes->comm);
+        seq_printf(file, "\"Usuario\": %d, \n", (int) processes->sessionid);
+        seq_printf(file, "\"Memory\": \"%d\", \n",__kuid_val(processes->real_cred->uid));
+        seq_printf(file, "\"Estado\": %ld} \n", processes->state);
 
-
-        list_for_each(p, &(proceso->children))
-        {
-            
-            //seq_printf(archivo, "    -------------------------------\n");
-            seq_printf(archivo, ",{\n");
-            //seq_printf(archivo, "   \"procesoh%i\":{\n", cont);
-            //seq_printf(archivo, "{\n");
+        list_for_each(p, &(processes->children)){
+            seq_printf(file, ",{\n");
             ts = *list_entry(p, struct task_struct, sibling);
-            seq_printf(archivo, "     \"Proceso padre\":%d,\n", proceso->pid);
-            seq_printf(archivo, "     \"PID\":%d, \n", ts.pid);
-            seq_printf(archivo, "     \"Nombre\":\"%s\",\n", ts.comm);
-            seq_printf(archivo, "     \"Usuario\": %d, \n", (int) proceso->sessionid);
-            seq_printf(archivo, "     \"Estado\":%ld \n", ts.state);
-            
-             seq_printf(archivo, "}\n");
+            seq_printf(file, "     \"processes padre\":%d,\n", processes->pid);
+            seq_printf(file, "     \"PID\":%d, \n", ts.pid);
+            seq_printf(file, "     \"Nombre\":\"%s\",\n", ts.comm);
+            seq_printf(file, "     \"Usuario\": %d, \n", (int) processes->sessionid);
+            seq_printf(file, "     \"Estado\":%ld \n", ts.state);
+            seq_printf(file, "}\n");
         }
-
     }
-    seq_printf(archivo, "],\n");
-    seq_printf(archivo, "\"arbol\":[\n");
-    
+
     seconditerative=false;
-    for_each_process(proceso)
-    {
+    seq_printf(file, "],\n");
+    seq_printf(file, "\"arbol\":[\n");
+    
+    for_each_process(processes){
         if(seconditerative){
-        seq_printf(archivo, ",");    
+        seq_printf(file, ",");    
         }else{
             seconditerative=true;
         }
 
-        seq_printf(archivo, "{\n");
-        seq_printf(archivo, "\"id\": %d, \n", proceso->pid);
-        seq_printf(archivo, "\"parentId\": %d ,\n",0);
-        seq_printf(archivo, "\"label\": \"%s\", \n", proceso->comm);
-        //seq_printf(archivo, "\"Usuario\": %d, \n", (int) proceso->sessionid);
-        //seq_printf(archivo, "\"Memory\": \"%ld\", \n",(long) proceso->mm->total_vm);
-        //seq_printf(archivo, "\"Estado\": %ld} \n", proceso->state);
-        seq_printf(archivo, "\"items\": %s \n", "[]");
-        seq_printf(archivo, "}\n");
-        list_for_each(p, &(proceso->children))
-        {
-            
-            seq_printf(archivo, ",{\n");
+        seq_printf(file, "{\n");
+        seq_printf(file, "\"id\": %d, \n", processes->pid);
+        seq_printf(file, "\"parentId\": %d ,\n",0);
+        seq_printf(file, "\"label\": \"%s\", \n", processes->comm);
+        seq_printf(file, "\"items\": %s \n", "[]");
+        seq_printf(file, "}\n");
+
+        list_for_each(p, &(processes->children)){
+            seq_printf(file, ",{\n");
             ts = *list_entry(p, struct task_struct, sibling);
-            seq_printf(archivo, "     \"id\":%d, \n", ts.pid);
-            seq_printf(archivo, "     \"label\":\"%s\",\n", ts.comm);
-            seq_printf(archivo, "     \"parentId\":%d,\n", proceso->pid);
-            //seq_printf(archivo, "     \"Usuario\": %d, \n", (int) proceso->sessionid);
-            //seq_printf(archivo, "     \"Estado\":%ld \n", ts.state);
-            seq_printf(archivo, "\"items\": %s \n", "[]");
-             seq_printf(archivo, "}\n");
-        }
-        
-        
+            seq_printf(file, "     \"id\":%d, \n", ts.pid);
+            seq_printf(file, "     \"label\":\"%s\",\n", ts.comm);
+            seq_printf(file, "     \"parentId\":%d,\n", processes->pid);
+            seq_printf(file, "\"items\": %s \n", "[]");
+            seq_printf(file, "}\n");
+        }             
     }
     
-    seq_printf(archivo, "]\n");
-
-    seq_printf(archivo, "}\n");
+    seq_printf(file, "]\n");
+    seq_printf(file, "}\n");
 
     return 0;
 }
@@ -115,8 +93,7 @@ static int to_open(struct inode *inode, struct file *file){
 //Verificar el Kernel con el comando uname -r
 
 //Si el kernel es 5.6 o mayor se usa la estructura proc_ops
-static struct proc_ops operations =
-{
+static struct proc_ops operations ={
     .proc_open = to_open,
     .proc_read = seq_read
 };
