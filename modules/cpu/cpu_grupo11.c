64a3f596a2566ea78e87e307fdbdbb370d8ae36d
Eduardo Catalán <mcatalan@cartacorp.net>
2023-06-06 20:54:49 -0600

    Escritura cpu

--- modules/cpu/cpu_grupo11.c
+++ modules/cpu/cpu_grupo11.c
@@ -1,28 +1,110 @@
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
+#include <asm/uaccess.h>
+#include <linux/hugetlb.h>
+#include <linux/sched/signal.h>
+#include <linux/sched.h>
 #include <linux/module.h>
 #include <linux/init.h>
-#include <linux/seq_file.h>
-#include <linux/mm.h>
-#include <linux/proc_fs.h>
 #include <linux/kernel.h>
-#include <linux/sysinfo.h>
+#include <linux/fs.h>
 
+struct list_head *p;
+struct task_struct *proceso, ts, *tsk;
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Monitor modulo CPU");
 MODULE_AUTHOR("Marvin Eduardo Catalán Véliz, Sara Paulina Medrano Cojulún");
 
-struct sysinfo inf;
 
-static int write_file(struct seq_file *file, void *v){
-    long total_mem, free_mem;
-    si_meminfo(&inf);
-    total_mem = (inf.totalram * 4 / 1024);
-    free_mem = (inf.freeram * 4 / 1024);
-    seq_printf(file, "{\n");
-    seq_printf(file, " \"MemoriaTotal\":%8lu,\n", total_mem);
-    seq_printf(file, " \"MemoriaLibre\":%8lu,\n", free_mem);
-    seq_printf(file, " \"MemoriaUsada\":%i\n", 100 - (free_mem * 100) / total_mem);
-    seq_printf(file, "}\n");
+static int write_file(struct seq_file *archivo, void *v){
+    int pros = 0;
+    int pros2 = 0;
+    seq_printf(archivo, "{\n");
+    seq_printf(archivo, "\"procesos\":[\n");
+
+    bool seconditerative=false;
+
+    for_each_process(proceso)
+    {
+
+        //seq_printf(archivo, "{\n\"proceso%i\":{\n", pros2);
+        if(seconditerative){
+        seq_printf(archivo, ",");    
+        }else{
+            seconditerative=true;
+        }
+
+        seq_printf(archivo, "{\n");
+        seq_printf(archivo, "\"PID\": %d, \n", proceso->pid);
+        seq_printf(archivo, "\"Nombre\": \"%s\", \n", proceso->comm);
+        seq_printf(archivo, "\"Usuario\": %d, \n", (int) proceso->sessionid);
+        //seq_printf(archivo, "\"Memory\": \"%lu\", \n",(unsigned long) proceso->mm->start_data);
+        //printf("ULONG_MAX   :   %lu\n", (unsigned long) ULONG_MAX);
+        seq_printf(archivo, "\"Memory\": \"%d\", \n",__kuid_val(proceso->real_cred->uid)); //__kuid_val(s->real_cred->uid)
+        seq_printf(archivo, "\"Estado\": %ld} \n", proceso->state);
+
+
+        list_for_each(p, &(proceso->children))
+        {
+            
+            //seq_printf(archivo, "    -------------------------------\n");
+            seq_printf(archivo, ",{\n");
+            //seq_printf(archivo, "   \"procesoh%i\":{\n", cont);
+            //seq_printf(archivo, "{\n");
+            ts = *list_entry(p, struct task_struct, sibling);
+            seq_printf(archivo, "     \"Proceso padre\":%d,\n", proceso->pid);
+            seq_printf(archivo, "     \"PID\":%d, \n", ts.pid);
+            seq_printf(archivo, "     \"Nombre\":\"%s\",\n", ts.comm);
+            seq_printf(archivo, "     \"Usuario\": %d, \n", (int) proceso->sessionid);
+            seq_printf(archivo, "     \"Estado\":%ld \n", ts.state);
+            
+             seq_printf(archivo, "}\n");
+        }
+
+    }
+    seq_printf(archivo, "],\n");
+    seq_printf(archivo, "\"arbol\":[\n");
+    
+    seconditerative=false;
+    for_each_process(proceso)
+    {
+        if(seconditerative){
+        seq_printf(archivo, ",");    
+        }else{
+            seconditerative=true;
+        }
+
+        seq_printf(archivo, "{\n");
+        seq_printf(archivo, "\"id\": %d, \n", proceso->pid);
+        seq_printf(archivo, "\"parentId\": %d ,\n",0);
+        seq_printf(archivo, "\"label\": \"%s\", \n", proceso->comm);
+        //seq_printf(archivo, "\"Usuario\": %d, \n", (int) proceso->sessionid);
+        //seq_printf(archivo, "\"Memory\": \"%ld\", \n",(long) proceso->mm->total_vm);
+        //seq_printf(archivo, "\"Estado\": %ld} \n", proceso->state);
+        seq_printf(archivo, "\"items\": %s \n", "[]");
+        seq_printf(archivo, "}\n");
+        list_for_each(p, &(proceso->children))
+        {
+            
+            seq_printf(archivo, ",{\n");
+            ts = *list_entry(p, struct task_struct, sibling);
+            seq_printf(archivo, "     \"id\":%d, \n", ts.pid);
+            seq_printf(archivo, "     \"label\":\"%s\",\n", ts.comm);
+            seq_printf(archivo, "     \"parentId\":%d,\n", proceso->pid);
+            //seq_printf(archivo, "     \"Usuario\": %d, \n", (int) proceso->sessionid);
+            //seq_printf(archivo, "     \"Estado\":%ld \n", ts.state);
+            seq_printf(archivo, "\"items\": %s \n", "[]");
+             seq_printf(archivo, "}\n");
+        }
+        
+        
+    }
+    
+    seq_printf(archivo, "]\n");
+
+    seq_printf(archivo, "}\n");
+
     return 0;
 }
 
