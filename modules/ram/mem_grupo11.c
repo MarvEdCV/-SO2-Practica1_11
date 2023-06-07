ca8951fe611f483103e37f476c45323d2bf5f576
Eduardo Catalán <mcatalan@cartacorp.net>
2023-06-06 20:34:06 -0600

    fix

--- modules/ram/mem_grupo11.c
+++ modules/ram/mem_grupo11.c
@@ -21,7 +21,7 @@ static int write_file(struct seq_file *file, void *v){
     seq_printf(file, "{\n");
     seq_printf(file, " \"MemoriaTotal\":%8lu,\n", total_mem);
     seq_printf(file, " \"MemoriaLibre\":%8lu,\n", free_mem);
-    seq_printf(file, " \"MemoriaUsada\":%i\n", (free_mem * 100) / total_mem);
+    seq_printf(file, " \"MemoriaUsada\":%i\n", 100 - (free_mem * 100) / total_mem);
     seq_printf(file, "}\n");
     return 0;
 }
