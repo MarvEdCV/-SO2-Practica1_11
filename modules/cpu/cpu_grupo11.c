595721fce04995934505f37777ea9c42db64daf7
Eduardo Catalán <mcatalan@cartacorp.net>
2023-06-06 20:38:31 -0600

    prueba escritura cpu

--- modules/cpu/cpu_grupo11.c
+++ modules/cpu/cpu_grupo11.c
@@ -1,18 +1,57 @@
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/seq_file.h>
+#include <linux/mm.h>
+#include <linux/proc_fs.h>
+#include <linux/kernel.h>
+#include <linux/sysinfo.h>
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Monitor modulo CPU");
 MODULE_AUTHOR("Marvin Eduardo Catalán Véliz, Sara Paulina Medrano Cojulún");
 
-static int mount_module(void)
+static int write_file(struct seq_file *file, void *v){
+    long total_mem, free_mem, consumed_mem;
+    si_meminfo(&inf);
+    total_mem = (inf.totalram * 4 / 1024);
+    free_mem = (inf.freeram * 4 / 1024);
+    seq_printf(file, "{\n");
+    seq_printf(file, " \"MemoriaTotal\":%8lu,\n", total_mem);
+    seq_printf(file, " \"MemoriaLibre\":%8lu,\n", free_mem);
+    seq_printf(file, " \"MemoriaUsada\":%i\n", 100 - (free_mem * 100) / total_mem);
+    seq_printf(file, "}\n");
+    return 0;
+}
+
+static int to_open(struct inode *inode, struct file *file){
+    return single_open(file, write_file, NULL);
+}
+
+//Verificar el Kernel con el comando uname -r
+
+//Si el kernel es 5.6 o mayor se usa la estructura proc_ops
+static struct proc_ops operations =
 {
+    .proc_open = to_open,
+    .proc_read = seq_read
+};
+
+/*Si el kernel es menor al 5.6 usan file_operations
+static struct file_operations operaciones =
+{
+    .open = al_abrir,
+    .read = seq_read
+};
+*/
+
+static int mount_module(void){
+    proc_create("cpu_grupo11", 0, NULL, &operations);
     printk(KERN_INFO "Hola mundo, somos el grupo 11 y este es el monitor de cpu\n");
     return 0;
 }
 
-static void disassemble_module(void)
-{
+static void disassemble_module(void){
+    remove_proc_entry("cpu_grupo11", NULL);
     printk(KERN_INFO "Sayonara mundo, somos el grupo 11 y este fue el monitor de cpu\n");
 }
 
